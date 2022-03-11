fe5d72a6812e5698288cd7bf9e2551b19559ea59
Kuldeep <ks684341@gmail.com>
2022-03-11 22:21:34 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Postlists from './components/Postlists';
+
+function App() {
+  return (
+    <div className="App">
+      <Postlists />
+    </div>
+  );
+}
+
+export default App;
